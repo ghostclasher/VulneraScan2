d8fd412518bc96082684172e9b4aabeffcd2a15b
ghostclasher <udaygaur325@gmail.com>
2026-02-08 21:32:18 +0530

    add vercel routing fix

--- vulnerascan-frontend/vite.config.js
+++ vulnerascan-frontend/vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  // ✅ IMPORTANT FOR VERCEL DEPLOYMENT
-  base: "/", 
-
+  base: "/",
   build: {
     outDir: "dist"
   }
